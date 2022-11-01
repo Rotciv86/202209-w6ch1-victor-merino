825d10fe8d7acb381b1204f2fd952e223f6fd22f
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-01 20:00:33 +0100

    Add loadToDos reducer in toDoSlice

---
+++ src/redux/slices/toDoSlice/toDoSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ToDos } from "../../../types";
+
+interface ToDoState {
+  list: ToDos;
+}
+
+const initialState: ToDoState = {
+  list: [],
+};
+
+const toDoSlice = createSlice({
+  name: "tasks",
+  initialState,
+  reducers: {
+    loadToDos: (currentState, action: PayloadAction<ToDos>) => ({
+      ...currentState,
+      list: [...currentState.list, ...action.payload],
+    }),
+  },
+});
+
+export const { loadToDos: loadToDosActionCreator } = toDoSlice.actions;
+
+export const toDoReducer = toDoSlice.reducer;
